d6143294440042ed1936bb6d71897e36a5551a1b
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2013-05-08 12:19:25 +0200

    Rebase from 'debian/groovy/octomap'

--- include/octomap/CountingOcTree.h
+++ include/octomap/CountingOcTree.h
@@ -1,10 +1,19 @@
+#ifndef OCTOMAP_COUNTING_OCTREE_HH
+#define OCTOMAP_COUNTING_OCTREE_HH
+
+// $Id$
+
+/**
+* OctoMap:
+* A probabilistic, flexible, and compact 3D mapping library for robotic systems.
+* @author K. M. Wurm, A. Hornung, University of Freiburg, Copyright (C) 2009.
+* @see http://octomap.sourceforge.net/
+* License: New BSD License
+*/
+
 /*
- * OctoMap - An Efficient Probabilistic 3D Mapping Framework Based on Octrees
- * http://octomap.github.com/
- *
- * Copyright (c) 2009-2013, K.M. Wurm and A. Hornung, University of Freiburg
+ * Copyright (c) 2009-2011, K. M. Wurm, A. Hornung, University of Freiburg
  * All rights reserved.
- * License: New BSD
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -30,9 +39,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
-#ifndef OCTOMAP_COUNTING_OCTREE_HH
-#define OCTOMAP_COUNTING_OCTREE_HH
 
 
 #include <stdio.h>
